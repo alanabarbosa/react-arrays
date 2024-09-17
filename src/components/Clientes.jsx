9414e515682e4696d8b6c4695b4ca68cdde4e5d5
Alana Barbosa <itsalanayall@hotmail.com>
2024-09-17 20:51:25 -0300

    Remove components

--- src/components/Clientes.jsx
+++ src/components/Clientes.jsx
@@ -1,51 +1,48 @@
 import React from 'react';
 
-const clientes = [
-    {
-      cliente: 'Luana',
-      idade: 27,
-      compras: [
-        { nome: 'Notebook', preco: 'R$ 2500' },
-        { nome: 'Geladeira', preco: 'R$ 3000' },
-        { nome: 'Smartphone', preco: 'R$ 1500' },
-      ],
-      ativa: true,
-    },
-    {
-      cliente: 'João',
-      idade: 35,
-      compras: [
-        { nome: 'Televisão', preco: 'R$ 2000' },
-        { nome: 'Máquina de Lavar', preco: 'R$ 1800' },
-        { nome: 'Câmera Fotográfica', preco: 'R$ 1200' },
-      ],
-      ativa: false,
-    },
-    {
-      cliente: 'Maria',
-      idade: 41,
-      compras: [
-        { nome: 'Ar-condicionado', preco: 'R$ 3500' },
-        { nome: 'Sofá', preco: 'R$ 2200' },
-        { nome: 'Micro-ondas', preco: 'R$ 400' },
-      ],
-      ativa: true,
-    },
-    {
-      cliente: 'Pedro',
-      idade: 29,
-      compras: [
-        { nome: 'Console de Videogame', preco: 'R$ 2800' },
-        { nome: 'Fritadeira Elétrica', preco: 'R$ 300' },
-        { nome: 'Smartwatch', preco: 'R$ 900' },
-      ],
-      ativa: false,
-    }
-  ];
-  
-
-
 const Clientes = () => { 
+    const clientes = [
+        {
+          cliente: 'Luana',
+          idade: 27,
+          compras: [
+            { nome: 'Notebook', preco: 'R$ 2500' },
+            { nome: 'Geladeira', preco: 'R$ 3000' },
+            { nome: 'Smartphone', preco: 'R$ 1500' },
+          ],
+          ativa: true,
+        },
+        {
+          cliente: 'João',
+          idade: 35,
+          compras: [
+            { nome: 'Televisão', preco: 'R$ 2000' },
+            { nome: 'Máquina de Lavar', preco: 'R$ 1800' },
+            { nome: 'Câmera Fotográfica', preco: 'R$ 1200' },
+          ],
+          ativa: false,
+        },
+        {
+          cliente: 'Maria',
+          idade: 41,
+          compras: [
+            { nome: 'Ar-condicionado', preco: 'R$ 3500' },
+            { nome: 'Sofá', preco: 'R$ 2200' },
+            { nome: 'Micro-ondas', preco: 'R$ 400' },
+          ],
+          ativa: true,
+        },
+        {
+          cliente: 'Pedro',
+          idade: 29,
+          compras: [
+            { nome: 'Console de Videogame', preco: 'R$ 2800' },
+            { nome: 'Fritadeira Elétrica', preco: 'R$ 300' },
+            { nome: 'Smartwatch', preco: 'R$ 900' },
+          ],
+          ativa: false,
+        }
+      ];    
     
     return (
         <>
